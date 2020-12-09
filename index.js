2f8c716e140d668434b56928b99a4fe3bcc04d69
vrishinvv <vrishinvv@gmail.com>
2020-12-09 20:49:57 +0530

    first commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 const config = require('./config.json');
 
 client.on('ready', () => {
-    console.log('The client is ready');
+    console.log('The client is ready!');
 });
 
 client.login(config.token);
