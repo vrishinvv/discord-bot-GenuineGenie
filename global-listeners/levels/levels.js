d59c3a2061fbfbc96c06c594e019c5aa7a5fce67
vrishinvv <vrishinvv@gmail.com>
2020-12-12 18:18:41 +0530

    refactored project with dels

--- global-listeners/levels/levels.js
+++
@@ -1,9 +0,0 @@
-const mongo = require('../../mongo');
-const updateXP = require('../../helper-snips/updateXP');
-module.exports = (client) => {
-    client.on('message', (message) => {
-        const { guild, member } = message;
-
-        updateXP(member.id, 23, message);
-    });
-};
